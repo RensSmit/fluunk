467f9a6f8b08bfd0e61f51c98c9a9d140e036627
Leon Westerveld <35258@ma-web.nl>
2023-03-26 04:29:05 +0200

    temperatuur + styling

--- website/index/js/main.js
+++ website/index/js/main.js
@@ -1,19 +1,58 @@
+/* klok */
 
-    /* klok */
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var d = today.getDate();
+    var y = today.getFullYear();
+    var month = today.getMonth() + 1;
+    h = checkTime(h);
+    m = checkTime(m);
+    document.getElementById("js--time").innerHTML = h + ":" + m;
+    document.getElementById("js--day").innerHTML = d + "-" + month + "-" + y;
+    var t = setTimeout(startTime, 500);
+}
 
-    function startTime() {
-        var today = new Date();
-        var h = today.getHours();
-        var m = today.getMinutes();
-        h = checkTime(h);
-        m = checkTime(m);
-        document.getElementById('time').innerHTML = h + ":" + m;
-        var t = setTimeout(startTime, 500);
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
     }
-    
-    function checkTime(i) {
-        if (i < 10) {
-            i = "0" + i;
-        }
-        return i;
-    }
+    return i;
+}
+
+/*temperature*/
+let weather = fetch("https://api.openweathermap.org/data/2.5/weather?lat=52.39093389936427&lon=4.856288216120264&units=metric&appid=630af0310ee841f7923146b451fabe63")
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(realData){
+        temp = realData.main.temp
+        min = realData.main.temp_min
+        max = realData.main.temp_max
+        like = realData.main.feels_like
+        weer = realData.weather[0].description
+        iconss = realData.weather[0].icon
+
+
+        const temperature = document.getElementById("js--temp")
+        temperature.innerText = temp + " °C"
+
+        const minmax = document.getElementById("js--minmax")
+        minmax.innerText = max + "° / " + min + "° feels like " + like + "°"
+
+
+        const weather = document.getElementById("js--weather")
+        weather.innerText = weer
+
+        const icon = document.getElementById("js--icon").setAttribute("src", "http://openweathermap.org/img/wn/" + iconss + "@2x.png")
+    })
+
+let getal = 18
+console.log(getal)
+
+const upButton = document.getElementById("js--up")
+const downButton = document.getElementById("js--down")
+const heat = document.getElementById("js--heat")
+
+
