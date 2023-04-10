1b00ed99c2de239a7a692c159f2bfc11e76ae890
Leon Westerveld <35258@ma-web.nl>
2023-04-10 18:02:47 +0200

    linked

--- website/login/php/index.php
+++ website/login/php/index.php
@@ -8,7 +8,7 @@
      <form action="login.php" method="post">
      	<h2>LOGIN</h2>
      	<?php if (isset($_GET['error'])) { ?>
-     		<p class="error"><?php echo $_GET['error']; ?></p>
+     		<p class="error"><?php echo $_POST['error']; ?></p>
      	<?php } ?>
      	<label>User Name</label>
      	<input type="text" name="uname" placeholder="User Name"><br>
